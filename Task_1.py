3ffe1cc1830e03b41406940216e40de19b18f153
java13048268 <java13048268@accenture.com>
2024-06-16 15:50:28 +0530

    Tasks added

--- Task_1.py
+++ Task_1.py
@@ -1,8 +1,14 @@
 # 1. Explain the differnce between the = operator and the == operator in Python.
 # Answer: = is assignment operator whereas == is compare operator
+a = 67
+b = 78
+
+print(a==b)
 
 # 2. What does the ** operator do in python and how is it used?
 # Answer: ** operator is the Exponentiation operator. It will do power operation.
+a = 2**3
+print(a)
 
 # 3. What does the ^ operator do in Python, and in what context is it commonly used?
 # Answer: ^ symbol is a Bitwise XOR operator
