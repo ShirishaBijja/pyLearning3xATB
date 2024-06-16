2723019d551b5558436124a3fedad702f6b50f78
java13048268 <java13048268@accenture.com>
2024-06-16 15:39:58 +0530

    new files added

---
+++ Task_1.py
@@ -0,0 +1,8 @@
+# 1. Explain the differnce between the = operator and the == operator in Python.
+# Answer: = is assignment operator whereas == is compare operator
+
+# 2. What does the ** operator do in python and how is it used?
+# Answer: ** operator is the Exponentiation operator. It will do power operation.
+
+# 3. What does the ^ operator do in Python, and in what context is it commonly used?
+# Answer: ^ symbol is a Bitwise XOR operator
---
+++ Task_2.py
@@ -0,0 +1,17 @@
+# Develop a Python script that calculate the sqaure and cube of a given number. num = 2, sq = 4, c= 8
+
+num = 2
+square = num ** 2
+cube = num ** 3
+print("Num is: ", num)
+print("Square of num: ", square)
+print("Cube of the num: ", cube)
+
+
+# Create a program that takes two numbers as input and prints whether the first number is greater then, less than, or equal to the second number.
+num1 = int(input("Enter the first number:\t"))
+num2 = int(input("\nEnter the second number:\t"))
+if num1 > num2:
+    print("Number1 is greater than Number2")
+else:
+    print("Number2 is greater than Number1")
